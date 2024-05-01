da40cf9c57d9cf61b9468426d9eb4c4823711492
Filippo Mameli <mameli93@gmail.com>
2024-05-01 16:10:50 +0200

    Update ThrottlingDetector module to handle high CPU usage and insert anomaly data into the database

--- lib/ThrottlingDetector.ex
+++ lib/ThrottlingDetector.ex
@@ -4,7 +4,8 @@ defmodule ThrottlingDetector do
   require Logger
 
   @time_window 10 * 60
-  @threshold 100
+  @peak_cpu_threshold 500
+  @high_cpu_usage_threshold 300
 
   # GenServer API
   def start_link(args \\ [], opts \\ []) do
@@ -38,17 +39,24 @@ defmodule ThrottlingDetector do
     |> Ecto.Query.where([sd], sd.insert_date_time > ^window_start_time)
     |> Sensors.Repo.all()
 
-    %{count: count, sum: sum} = sd_list |> Enum.reduce(%{sum: 0, count: 0}, fn sd, acc ->
-      %{sum: acc.sum + sd.sensor_mean_value, count: acc.count + 1}
+    %{count: count, sum: sum, peak: peak} = sd_list |> Enum.reduce(%{sum: 0, count: 0, peak: 0}, fn sd, acc ->
+      %{sum: acc.sum + sd.sensor_mean_value, count: acc.count + 1, peak: max(acc.peak, sd.sensor_peak_value)}
     end)
 
     mean = sum / count
 
-    if mean > @threshold do
+    if mean > @high_cpu_usage_threshold or peak > @peak_cpu_threshold  do
       Logger.info("High CPU usage detected")
+      ad = %Sensors.AnomalyData{
+        flag_high_cpu_usage: true,
+        flag_high_gpu_usage: false,
+        insert_date_time: latest_insert_time
+      }
+
+      Sensors.Repo.insert(ad)
     end
 
-    sd_list |> Enum.any?(fn sd -> sd.sensor_peak_value > @threshold end) && Logger.info("Throttling detected")
+
 
 
   end
